4a7b3eb8316f6923a7ef5d1b2ea5af26d41a0d75
quincymorgan104 <quincymorgan@icloud.com>
2015-08-10 13:21:51 +0000

    final commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'app/public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'nuggetsrfam'
   end
   
   get '/' do
@@ -16,22 +18,39 @@ class ApplicationController < Sinatra::Base
   get '/new_gif' do
      erb :new_gif
   end
+  
   get '/gifeed' do
     @gifs=Gif.all
-    erb :gif_feed
+    erb :gifeed
   end
     
   get '/signup'do
       erb :signup
   end
+  
   post '/signup' do
     @user= User.new(:username => params[:username],:email => params[:email], :password => params[:password])
     @user.save
-    redirect '/new_gif'
+    redirect '/login'
   end
-  
+  get '/login' do
+    erb :login
+  end
+  get '/signout' do
+     session[:user_id]= nil
+    redirect '/'
+  end
+  post '/login' do
+    @user= User.find_by(:username => params[:username],:password => params[:password])
+    if @user==nil
+    redirect '/signup'
+    else 
+      session[:user_id]=@user.id
+      redirect '/gifeed'
+   end
+  end
   post '/new_gif' do
-    @user= User.find_by(:username => params[:username])
+    @user= User.find_by(:id =>  session[:user_id])
     @gif= Gif.new(:user_id => @user.id, :url => params[:url])
     @gif.save
    
